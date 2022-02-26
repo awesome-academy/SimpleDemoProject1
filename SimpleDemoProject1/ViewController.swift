09bc2e619d609e1039a5219fd63883fb361aceb0
namtn <trinh.ngoc.nam@sun-asterisk.com>
2022-02-26 13:50:35 +0700

    Initial Commit

---
+++ SimpleDemoProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleDemoProject1
+//
+//  Created by trinh.ngoc.nam on 26/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
